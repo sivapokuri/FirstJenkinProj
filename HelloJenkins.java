10430ba4843357b03507d9369a815ae90c5897a4
sivapokuri <pokuri86@gmail.com>
2019-09-17 07:47:12 -0400

    Create HelloJenkins.java
    First Jenkins

---
+++ HelloJenkins.java
@@ -0,0 +1,8 @@
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloJenkins {
+  public static void main(String[] args) {
+    System.out.println("Hello, Jenkins");
+  }
+}
